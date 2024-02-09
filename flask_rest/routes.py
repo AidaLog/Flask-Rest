49816952485e8eee135971b81e6032b7f23f83aa
eddygulled <edgargulay69@gmail.com>
2024-02-10 00:09:42 +0300

    installed flask rest

--- flask_rest/routes.py
+++ flask_rest/routes.py
@@ -1,6 +1,12 @@
 # routes.py
 from flask_rest import app
+from flask_restful import Resource, Api
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+
+api = Api(app)
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
